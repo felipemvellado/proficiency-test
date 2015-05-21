9c09a2a616cf3ec7a4b5d4caac5192816cb48390
Felipe Mota <sanomota@gmail.com>
2015-05-21 22:50:58 +0000

    Create a seed file to populate the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,93 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Creating courses (open and closed)
+Course.create(
+  name: 'Android e AppInventor',
+  description: 'Construa aplicativos para seu android sem...', status: 0)
+
+Course.create(
+  name: 'Desenvolvimento em Windows Phone',
+  description: 'Aprenda a desenvolver um app para Windows ...', status: 0)
+
+Course.create(
+  name: 'Virtualização com Hyper-V',
+  description: 'Aprenda a configurar esta funcionalidade d...', status: 1)
+
+Course.create(
+  name: 'Sharepoint Server 2013',
+  description: 'Aprenda sobre a arquitetura do Sharepoint ...', status: 1)
+
+Course.create(
+  name: 'Big Data',
+  description: 'Aprenda sobre a tecnologia que está mudand...', status: 1)
+
+Course.create(
+  name: 'Introdução à Lógica de Programação',
+  description: 'Aprenda o que é a lógica de programação so...', status: 0)
+
+Course.create(
+  name: 'Fundamentos do Empreendedorismo',
+  description: 'Sou empreendedor? Como conseguir financiam...', status: 0)
+
+Course.create(
+  name: 'Wordpress e Windows Azure',
+  description: 'Coloque sua startup no ar na nuvem da Micr...', status: 1)
+
+Course.create(
+  name: 'SQL Server 2012',
+  description: 'Aprenda sobre as novidades no SQL Server ...', status: 0)
+
+Course.create(
+  name: 'Cloud Computing',
+  description: 'Entenda o que é cloud computing e os model...', status: 1)
+
+Course.create(
+  name: 'Azure Web Apps',
+  description: 'Conheça o serviço na nuvem que permite cri...', status: 1)
+
+Course.create(
+  name: 'Acive Directory',
+  description: 'Aprenda a trabalhar com o serviço de diret...', status: 0)
+
+# Creating students (registered and unregistered)
+Student.create(
+  name: 'Xico Fausto Naves',
+  register_number: '25231291331119028993984', status: 0)
+
+Student.create(
+  name: 'Marília Israel Porciúncula',
+  register_number: '24354678908921312937313917131310138', status: 0)
+
+Student.create(
+  name: 'Aguinaldo Boaventura Tristão',
+  register_number: '0912930193132382383', status: 0)
+
+Student.create(
+  name: 'Adérito Napoleão Castelo Branco',
+  register_number: '222292298481012100000000', status: 1)
+
+Student.create(
+  name: 'Felícia Honório Coutinho',
+  register_number: '12912912819322747635839356', status: 1)
+
+Student.create(
+  name: 'Tânia Hermenegildo Vilariça',
+  register_number: '201921371907109173', status: 1)
+
+Student.create(
+  name: 'Salomão Guadalupe Hilário',
+  register_number: '2121210909323748957986785796', status: 0)
+
+Student.create(
+  name: 'Sabrina Reginaldo Castilho',
+  register_number: '4565464349324023487209402401', status: 1)
+
+Student.create(
+  name: 'Quintino Iolanda Salomão',
+  register_number: '901932912731390172313123775858575847', status: 0)
+
+Student.create(
+  name: 'Gregório Tatiana Valério',
+  register_number: '193023129329381231310973', status: 0)
+
+Student.create(
+  name: 'Samuel Tércio Regalado',
+  register_number: '131231301392391231232133123134', status: 0)
